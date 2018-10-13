0f850f63ba324733d5606ff58da432d2cca064a6
Wonn Jen <38198099+Booxworm@users.noreply.github.com>
2018-10-13 20:19:25 +0800

    Adding image upload capability

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express"),
 bodyParser	  = require("body-parser"),
-		app   = express();
+multer		  = require("multer"),
+fs			  = require("fs"),
+db			  = require("./models/db.js");
+app			  = express(),
+upload		  = multer({ dest : "./uploads" });
 
 app.use(express.static(__dirname + "/views"));
 app.use(express.static(__dirname + "/public"));
@@ -12,10 +16,24 @@ app.get("/", function(req, res){
 	res.sendFile("index.html");
 });
 
-app.post("/", function(req,res){
-	var data = req.body
-	console.log(data)
+app.post("/", upload.single("jpeg"), function(req,res){
+	var data = req.body;
+	console.log(data);
+	var newEntry = new db.Crim();
+	newEntry.img = fs.readFileSync(req.file.path);
+	newEntry.save();
+
 	res.render("data", {data})
+});
+
+app.get('/testdb/', function(req, res){
+    db.Crim.find()
+    .then(function(todo){
+		res.status(201).json(todo);
+	})
+	.catch(function(err){
+		res.send(err);
+	});
 });
 
 app.listen(8100, ()=> {console.log("listening on 8100")}
